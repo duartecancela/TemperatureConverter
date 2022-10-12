ddd76d1a316f24f8bf12fda065431355ee8f7110
Duarte Cancela <duartecancela@gmail.com>
2022-10-12 12:44:10 +0100

    add button

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
@@ -33,38 +34,44 @@ class MainActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
-        val insertedTemperature : String  = editTextInsertTemp.text.toString()
-        val insertedTemperatureFloat : Float = insertedTemperature.toFloat()
-        var temperatureResult = 0.0
+
+        val buttonConvert = findViewById<Button>(R.id.buttonConvert)
 
         spinner.onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
                 // An item was selected. You can retrieve the selected item using
                 // parent.getItemAtPosition(pos)
-//                Toast.makeText(this@MainActivity, parent.getItemAtPosition(pos).toString(),
-//                    Toast.LENGTH_SHORT).show()
 
-                if(parent.getItemAtPosition(pos).toString() == "Fahrenheit to Celsius"){
+                val insertedTemperature : String  = editTextInsertTemp.text.toString()
+                val insertedTemperatureFloat : Float = insertedTemperature.toFloat()
+                var temperatureResult: Double
 
-                    temperatureResult = insertedTemperatureFloat - 32 / 1.80
+                buttonConvert.setOnClickListener {
 
-                    textViewConverted.text = temperatureResult.toString()
+                    if(parent.getItemAtPosition(pos).toString() == "Fahrenheit to Celsius"){
 
-                    Toast.makeText(this@MainActivity, "F to C",
-                        Toast.LENGTH_SHORT).show()
+                        temperatureResult = insertedTemperatureFloat - 32.0 / 1.80
 
+                        textViewConverted.text = temperatureResult.toString()
+
+                        Toast.makeText(this@MainActivity, "F to C",
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    if(parent.getItemAtPosition(pos).toString() == "Celsius to Fahrenheit"){
+
+                        temperatureResult = (insertedTemperatureFloat * 1.8) / 32.0
+
+                        textViewConverted.text = temperatureResult.toString()
+
+                        Toast.makeText(this@MainActivity, "C to F",
+                            Toast.LENGTH_SHORT).show()
+                    }
                 }
 
-                if(parent.getItemAtPosition(pos).toString() == "Celsius to Fahrenheit"){
 
-                    temperatureResult = (insertedTemperatureFloat * 1.8) / 32
-
-                    textViewConverted.text = insertedTemperatureFloat.toString()
-
-                    Toast.makeText(this@MainActivity, "C to F",
-                        Toast.LENGTH_SHORT).show()
-                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
